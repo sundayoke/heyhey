5e2b87886d9e00e259f855286237ac186d8b2fe9
sundayoke <sunday.oke@gmail.com>
2022-09-10 14:06:59 +0000

    a few more math functions

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ print(type(9.9+1.1))
 print(2 ** 3) #To the pwer of Two to the power of 3
 print (5 / 4) # Simple division resulting in a float
 print (5 // 4) # Double divide, rounded down to and integer
-print(5 % 4) # whats the remainder of this division
+print(5 % 4) # Modulo, whats the remainder of this division
+
+# Math functions
+print(round(3.8))
+print(abs(-20))
