512fe13410628e66095101b72ff0c2771588aaa6
sundayoke <sunday.oke@gmail.com>
2022-09-10 13:46:28 +0000

    dealing with arithmetic

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = input('What is your name?')
-print('Hello ' + name)
-answer = input('How are you ' + name + '? ')
-print(answer)
+#name = input('What is your name?')
+#print('Hello ' + name)
+#answer = input('How are you ' + name + '? ')
+#print(answer)
+print(type(9.9+1.1))
+
+print(2 ** 3) #To the pwer of Two to the power of 3
+print (5 / 4) # Simple division resulting in a float
+print (5 // 4) # Double divide, rounded down to and integer
+print(5 % 4) # whats the remainder of this division
